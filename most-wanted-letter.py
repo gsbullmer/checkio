619d7cba88d8a5c366ab6dbb76915c361699474a
Greg Bullmer <gsbullmer@gmail.com>
2017-07-17 10:11:52 -0500

    feat: First mission of Home

---
+++ most-wanted-letter.py
@@ -0,0 +1,19 @@
+def checkio(text):
+    """
+    Returns the most-occurring letter in `text`, and in the event of a tie,
+    the letter that occurs first in the alphabet.
+    """
+    letters = sorted({l for l in text.lower() if l.isalpha()})
+    return max(letters, key=text.lower().count)
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio("Hello World!") == "l", "Hello test"
+    assert checkio("How do you do?") == "o", "O is most wanted"
+    assert checkio("One") == "e", "All letter only once."
+    assert checkio("Oops!") == "o", "Don't forget about lower case."
+    assert checkio("AAaooo!!!!") == "a", "Only letters."
+    assert checkio("abe") == "a", "The First."
+    print("Start the long test")
+    assert checkio("a" * 9000 + "b" * 1000) == "a", "Long."
+    print("The local tests are done.")
