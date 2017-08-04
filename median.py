6e8dc178005b9cb2f7e183d4437d1d984e860e51
Greg Bullmer <gsbullmer@gmail.com>
2017-08-03 23:45:26 -0500

    feat: Add mission 1 of O'Reilly

---
+++ median.py
@@ -0,0 +1,20 @@
+def checkio(data):
+    """
+    Return the median value of `data`
+    """
+    data.sort()
+    median_index = len(data) // 2
+    if len(data) % 2 is 0:
+        return sum(data[median_index-1:median_index+1]) / 2
+    else:
+        return data[median_index]
+
+#These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    assert checkio([1, 2, 3, 4, 5]) == 3, "Sorted list"
+    assert checkio([3, 1, 2, 5, 3]) == 3, "Not sorted list"
+    assert checkio([1, 300, 2, 200, 1]) == 2, "It's not an average"
+    assert checkio([3, 6, 20, 99, 10, 15]) == 12.5, "Even length"
+    print("Start the long test")
+    assert checkio(list(range(1000000))) == 499999.5, "Long."
+    print("The local tests are done.")
